d391f979bdc7b7232eb4cf3997ef37f5c1a01ff3
Sandeep Patel <sandeep.patel4488@gmail.com>
2025-03-25 12:06:06 +0530

    Throw an Error for Negative Numbers

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -21,3 +21,7 @@ test("supports custom delimiters", () => {
     expect(add("//;\n1;2")).toBe(3);
     expect(add("//|\n2|3|4")).toBe(9);
 });
+
+test("throws error for negative numbers", () => {
+    expect(() => add("1,-2,3,-4")).toThrow("negative numbers not allowed -2,-4");
+});
