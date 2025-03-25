33b6594290ff309c4ca3834591acf53a48f6ce8e
Sandeep <sandeep.patel4488@gmail.com>
2025-03-25 09:33:33 +0530

    Initial commit : Project setup and add method added with basic empty and single string functionality

---
+++ src/stringCalculator.test.ts
@@ -0,0 +1,10 @@
+import { add } from './stringCalculator';
+
+test('returns 0 for an empty string', () => {
+    expect(add("")).toBe(0);
+});
+
+test('returns the number itself for a single number', () => {
+    expect(add("1")).toBe(1);
+    expect(add("5")).toBe(5);
+});
